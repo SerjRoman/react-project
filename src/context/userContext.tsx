1268307ae5c26e8537afb24e030ef5e05728de7a
AlexeyTarasov77 <alexxey2707@gmail.com>
2025-03-11 18:23:55 +0100

    Add comments for user context

--- src/context/userContext.tsx
+++ src/context/userContext.tsx
@@ -12,89 +12,95 @@ interface IUser {
 
 interface IUserContext {
     user: IUser | null
-    login: (email: string , password: string) => void
-    register: (username: string, email:string, password: string, image: string) => void
+    login: (email: string, password: string) => void
+    register: (username: string, email: string, password: string, image: string) => void
 
 }
 
 
-
+// дефолтное значение для контекста
 const initialValue: IUserContext = {
     user: null,
-    login: (email, password) => {},
-    register: (username, email, password, image) => {}
+    login: (email, password) => { },
+    register: (username, email, password, image) => { }
 }
 
 const userContext = createContext<IUserContext>(initialValue)
 
-export function useUserContext(){
+// обертка над хуком для получения контекста юзера
+export function useUserContext() {
     return useContext(userContext);
 }
 
 interface IUserContextProvider {
-    children : ReactNode
+    children: ReactNode
 
 }
 
 
 
-
+// обертка над провайдером для контекста юзера
 export function UserContextProvider(props: IUserContextProvider) {
-    const {children} = props
-    const [user , setUser] = useState<IUser | null>(null)
+    const { children } = props
+    const [user, setUser] = useState<IUser | null>(null)
 
-    async function getUser(token : string){
-        try{
-            const response = await fetch("http://localhost:8000/api/user/me",{
+    // Функция для получения юзера с api по токену
+    async function getUser(token: string) {
+        try {
+            const response = await fetch("http://localhost:8000/api/user/me", {
                 headers: {
                     "Authorization": `Bearer ${token}`
                 }
             })
-        } catch{    
+        } catch {
 
         }
     }
 
 
-    async function login(email: string, password : string){
-        try{
-            const response = await fetch("http://localhost:8000/api/user/login",{
+    // Функция для авторизации юзера, отправляет запрос на api и в случае успеха - должна сохранять полученный токен
+    async function login(email: string, password: string) {
+        try {
+            const response = await fetch("http://localhost:8000/api/user/login", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
                 },
-                body: JSON.stringify({email : email, password : password})
+                body: JSON.stringify({ email: email, password: password })
             })
             const result: Response<string> = await response.json()
-            if (result.status == "error"){
+            if (result.status == "error") {
                 console.log(result.message)
-            }else {
-                
+            } else {
+
             }
-        } catch{
-            
+        } catch {
+
         }
     }
-    async function register(username: string, email:string, password: string, image: string) {
+
+    // функция для регистрации, обрабатывает полученный ответ и сохраняет авторизационный токен (по крайней мере должна)
+    async function register(username: string, email: string, password: string, image: string) {
         try {
             const response = await fetch("http://localhost:8000/api/user/registration", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
                 },
-                body: JSON.stringify({username:username, email:email, password:password, image:image}),
+                body: JSON.stringify({ username: username, email: email, password: password, image: image }),
             })
             const result: Response<string> = await response.json()
-            
-            if (result.status === "error"){
+
+            if (result.status === "error") {
                 console.log(result.message)
-            }else {
-                
+            } else {
+
             }
-            
-        }catch{
+
+        } catch {
 
         }
     }
-    return<userContext.Provider value = {{user: user }}>{children}</userContext.Provider>
+    // возвращаем провайдер связанный с самим контекстом, передавая созданные функции
+    return <userContext.Provider value={{ user: user }}>{children}</userContext.Provider>
 }
--- src/shared/types/response.ts
+++ src/shared/types/response.ts
@@ -1,9 +1,10 @@
+// Набор типов для типизации ответа сервера
 
-export interface IOkWithData<T>{
+export interface IOkWithData<T> {
     status: "ok",
     data: T
 }
-export interface IError{
+export interface IError {
     status: "error",
     message: string
 }
